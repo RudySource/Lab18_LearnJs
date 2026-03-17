767c1682866c2a3d6bd372c4036e8e18950e2a19
Rud3us <rodionpylnev@yandex.ru>
2026-03-17 10:39:28 +0300

    Step 7: checking data types with typeof

--- main.js
+++ main.js
@@ -1,50 +1,54 @@
-let age = 20
-let name = 'Denis'
-let isStudent = true
-console.log('Name: ', name, '\nAge: ', age, '\nIs student: ', isStudent)
-console.log('-----------------------------')
-let value = 19
-console.log('Value: ', value)
-value = 'Now I am a string'
-console.log('Value: ', value)
-value = true
-console.log('Value: ', value)
-console.log('-----------------------------')
-function sum(a, b) {
-	return a + b
-}
+// let age = 20
+// let name = 'Denis'
+// let isStudent = true
+// console.log('Name: ', name, '\nAge: ', age, '\nIs student: ', isStudent)
+// console.log('-----------------------------')
+// let value = 19
+// console.log('Value: ', value)
+// value = 'Now I am a string'
+// console.log('Value: ', value)
+// value = true
+// console.log('Value: ', value)
+// console.log('-----------------------------')
+// function sum(a, b) {
+// 	return a + b
+// }
 
-let multiply = function (x, y) {
-	return x * y
-}
+// let multiply = function (x, y) {
+// 	return x * y
+// }
 
-console.log(sum(5, 3))
+// console.log(sum(5, 3))
 
-let now = new Date()
-let birthday = new Date('1995-12-17')
+// let now = new Date()
+// let birthday = new Date('1995-12-17')
 
-let a = 10
-let b = 3
+// let a = 10
+// let b = 3
 
-console.log(a + b)
-console.log(a - b)
-console.log(a * b)
-console.log(a / b)
-console.log('-----------------------------')
-console.log(10 + '5') // "105"
-console.log('10' - 5) // 5
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log('-----------------------------')
+// console.log(10 + '5') // "105"
+// console.log('10' - 5) // 5
 
-const numbersArray = [1, 2, 3]
-// Можно изменять элементы:
-numbersArray[0] = 10
-console.log(numbersArray) // [10, 2, 3]
-// // Нельзя переназначить:
-// numbersArray = [5, 6, 7] // Ошибка!
+// const numbersArray = [1, 2, 3]
+// // Можно изменять элементы:
+// numbersArray[0] = 10
+// console.log(numbersArray) // [10, 2, 3]
+// // // Нельзя переназначить:
+// // numbersArray = [5, 6, 7] // Ошибка!
 
-const persons = { name: 'Denis', age: 18 }
-// Можно изменять свойства:
-persons.age = 50
-persons.city = 'Volgograd'
-console.log(persons) // { name: "Denis", age: 50, city: "Volgograd" }
-// // Нельзя переназначить:
-// person = { name: 'Stas' } // Ошибка!
+// const persons = { name: 'Denis', age: 18 }
+// // Можно изменять свойства:
+// persons.age = 50
+// persons.city = 'Volgograd'
+// console.log(persons) // { name: "Denis", age: 50, city: "Volgograd" }
+// // // Нельзя переназначить:
+// // person = { name: 'Stas' } // Ошибка!
+let newPrice = 42
+console.log('New price: ', newPrice, '\nType of newPrice: ', typeof newPrice)
+newPrice = 'Forty-two'
+console.log('New price: ', newPrice, '\nType of newPrice: ', typeof newPrice)
